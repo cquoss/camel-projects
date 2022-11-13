92718d0c6b87c3dabdd18c90041e6b0bf00e62bd
Clemens Quoss <clemens@quoss.de>
2022-11-13 09:41:09 +0100

    Share http wip.

--- http/src/main/java/de/quoss/camel/http/Server.java
+++ http/src/main/java/de/quoss/camel/http/Server.java
@@ -2,15 +2,22 @@ package de.quoss.camel.http;
 
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
 
 public class Server {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Server.class); 
+    
     private void run() throws Exception {
+        final String methodName = "run()";
         final InetSocketAddress address = new InetSocketAddress(53537);
         final HttpServer server = HttpServer.create(address, 10);
         final HttpContext context = server.createContext("/", exchange -> {
+            LOGGER.info("{} [exchange={}]", methodName, exchange);
+            exchange.
             // sleep 5 minutes
             try {
                 Thread.sleep(300000L);
@@ -19,6 +26,7 @@ public class Server {
             }
             exchange.close();
         });
+        LOGGER.info("{} [context={}]", methodName, context);
         server.start();
     }
     
--- http/src/main/java/de/quoss/camel/http/route/ToVoid.java
+++ http/src/main/java/de/quoss/camel/http/route/ToVoid.java
@@ -1,7 +1,6 @@
 package de.quoss.camel.http.route;
 
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
-import org.apache.camel.component.http.HttpComponent;
 
 public class ToVoid extends EndpointRouteBuilder {
 
@@ -13,7 +12,7 @@ public class ToVoid extends EndpointRouteBuilder {
     public void configure() {
 
         // configure http component - set socket timeout to 1 minute
-        ((HttpComponent) getContext().getComponent("http")).setSocketTimeout(60000);
+        // ((HttpComponent) getContext().getComponent("http")).setSocketTimeout(60000);
         
         from(timer(ROUTE_ID).repeatCount(1L))
                 .routeId(ROUTE_ID)
